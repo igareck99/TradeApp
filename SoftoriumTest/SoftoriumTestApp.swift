69e7470a4b30bbf438444371fba5a3492d937634
Igareck99 <igor9.i@yandex.ru>
2023-11-22 03:34:13 +0300

    Initial Commit

---
+++ SoftoriumTest/SoftoriumTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoftoriumTestApp.swift
+//  SoftoriumTest
+//
+//  Created by Игорь Коноваленко on 22.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SoftoriumTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
